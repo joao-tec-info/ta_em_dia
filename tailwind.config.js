ec8d618cb74ee744896c6bf19553cea7523ea231
joao-tec-info <joaoemanuelcarloslima2201@gmail.com>
2025-10-21 08:46:13 -0300

    indentidade visual

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.js",
     "./src/styles/**/*.css"
   ],
-  theme: { extend: {} },
+  theme: { extend: {
+    colors: {
+        'green-haze': '#009c46',
+        'japanese-laurel': '#037c17',
+        'green-vogue': '#012056',
+        'black-squeeze': '#f2f9fa',
+        'ocean-green': '#43a680',
+        'ship-cove': '#8098bb',
+        'black-pearl': '#030519',
+      },
+
+
+  } },
   plugins: [],
 }
 
